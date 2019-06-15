9b9104a6541f9ba05ceaa89b41e5036989220e05
Ficzer <filipflorczyk@gmail.com>
2019-06-15 22:11:19 +0200

    Uranium Boot Loader Hello world

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,6 +1,35 @@
-; infinite loop (e9 fd ff)
-loop:
-	jmp loop
+
+mov ah, 0x0e ; tty mode
+mov al, 'U'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 'i'
+int 0x10
+mov al, 'u'
+int 0x10
+mov al, 'm'
+int 0x10
+mov al, 'B'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, '-'
+int 0x10
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+
+jmp $ ; jump to current, adress infinite loop
 	
 ; fill with 510 zeros - the size of previous code
 times 510-($-$$) db 0
